6a77eb410566b47621f31a94fdd29ece663855b3
Andrew Evans <AEvans@seamsglobal.com>
2020-02-11 16:03:47 +0000

    Using a role to log in

--- test/tests/login-test.ts
+++ test/tests/login-test.ts
@@ -1,11 +1,13 @@
-import LoginUtility from '../utilities/login-utility'
+import TestUser from '../utilities/test-user'
 import Settings from '../settings';
+import HomePage from '../page-model/home-page';
 
 fixture `Login Tests`
     .page(Settings.appUrl);
 
 test('should be able to log in, see the home page, and then log out', async t => {
-    await LoginUtility.login(t)
-        
+    await t
+        .useRole(TestUser)
+        .expect(HomePage.welcomeMessage.innerText).eql("Hello Test User, you are logged in!")
 });
 
